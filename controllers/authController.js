2e967667a19146e92a97140cac4da7828bad1cfe
tywest121 <tyresewest25@gmail.com>
2023-11-28 17:16:21 -0500

    Hello

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,27 +1,52 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
-module.exports.signup_get = (req,res) => {
-    res.render('signup');
+// handle errors
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { email: '', password: '' };
+
+  // duplicate email error
+  if (err.code === 11000) {
+    errors.email = 'that email is already registered';
+    return errors;
+  }
+
+  // validation errors
+  if (err.message.includes('user validation failed')) {
+    // console.log(err);
+    Object.values(err.errors).forEach(({ properties }) => {
+      // console.log(val);
+      // console.log(properties);
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
 }
 
-module.exports.login_get = (req,res) => {
-    res.render('login');
+// controller actions
+module.exports.signup_get = (req, res) => {
+  res.render('signup');
 }
-
-module.exports.signup_post = async (req,res) => {
-    const {email,password}=req.body;
-
-    try {
-        const user = await User.create({email,password});
-        res.status(201).json(user);
-    } catch (err) {
-        console.log(err);
-        res.status(400).send('error, user not created');
-    }
+module.exports.login_get = (req, res) => {
+  res.render('login');
 }
+module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  }
+  catch(err) {
+    console.log(err);
+    res.status(400).send('error, user not created');
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
 
-module.exports.login_post =async (req,res)=>{
-    const {email,password}=req.body;
-    console.log(email,password);
-    res.send('user login');
+}
+module.exports.login_post = async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+  res.send('user login');
 }
