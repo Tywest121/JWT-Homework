214c6f1d7c678db2708759af1e73e45aa28a394a
tywest121 <tyresewest25@gmail.com>
2023-11-28 18:11:21 -0500

    Mine

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,17 @@
 const User = require("../models/User");
+const jwt = require('jsonwebtoken');
 
 // handle errors
 const handleErrors = (err) => {
   console.log(err.message, err.code);
   let errors = { email: '', password: '' };
-
+ 
   // duplicate email error
   if (err.code === 11000) {
     errors.email = 'that email is already registered';
     return errors;
   }
-
+  
   // validation errors
   if (err.message.includes('user validation failed')) {
     // console.log(err);
@@ -20,9 +21,17 @@ const handleErrors = (err) => {
       errors[properties.path] = properties.message;
     });
   }
-
+  
   return errors;
 }
+
+// create json web token
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, 'net ninja secret', {
+    expiresIn: maxAge
+  });
+};
 
 // controller actions
 module.exports.signup_get = (req, res) => {
@@ -33,17 +42,20 @@ module.exports.login_get = (req, res) => {
 }
 module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
+
   try {
     const user = await User.create({ email, password });
     res.status(201).json(user);
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: user._id });
   }
   catch(err) {
-    console.log(err);
-    res.status(400).send('error, user not created');
+    
     const errors = handleErrors(err);
     res.status(400).json({ errors });
   }
-
+ 
 }
 module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
