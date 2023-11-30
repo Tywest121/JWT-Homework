323ec7aec6e1c70af7d5e558b2aa3c7efb453cf1
tywest121 <tyresewest25@gmail.com>
2023-11-29 20:00:19 -0500

    Mine

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 
 const requireAuth = (req, res, next) => {
@@ -18,4 +19,26 @@ const requireAuth = (req, res, next) => {
     res.redirect('/login');
   }
 };
-module.exports = { requireAuth };
+
+// check current user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, 'net ninja secret', async (err, decodedToken) => {
+      if (err) {
+        res.locals.user = null;
+        next();
+      } else {
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+
+module.exports = { requireAuth, checkUser };
